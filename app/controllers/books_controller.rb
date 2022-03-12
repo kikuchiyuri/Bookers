f4d382412f0f4cd5f78a51744d5487c7884e07f3
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-03-12 22:15:29 +0000

    [Add]updateアクションのバリデーション（エラーでる）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,8 @@
 class BooksController < ApplicationController
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
+    book = Book.new(book_params)
+    if book.save
       redirect_to book_path(book.id)
     else
       @books = Book.all
@@ -25,8 +25,12 @@ class BooksController < ApplicationController
 
   def update
     book =Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      @book =Book.find(params[:id])
+      render edit
+    end
   end
 
   def destroy
