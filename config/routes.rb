01dec7a01bd617debb597f9f2fe7ff2102e5e402
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-03-12 07:43:46 +0000

    [Add]一覧画面の作成、投稿・一覧画面を同じ画面に編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index'
   get 'books' => 'books#new'
   post 'books' => 'books#create'
-  get 'books/index'
+
   get 'books/show'
   get 'books/edit'
   get '/' => 'homes#top'
