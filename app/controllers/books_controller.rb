40af9ba89456ac83d014bc36c1755a2af60c668a
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-03-12 14:00:41 +0000

    [Update]バリデーションのエラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       redirect_to book_path(book.id)
     else
+      @books = Book.all
       render :index
     end
   end
