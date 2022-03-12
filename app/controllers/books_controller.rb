23c1ec13408685b34dd12e0090b28ec007d0b36d
kikuchiyuri <100644032+kikuchiyuri@users.noreply.github.com>
2022-03-12 13:46:42 +0000

    [Add]バリデーション機能の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,11 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
   end
 
   def index
